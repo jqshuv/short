8e5fa6eadcc6c2bec3588a66cae2bec902b03693
Joshua <schmitt@unately.com>
2024-10-16 01:27:22 +0200

    refactor: update release-it configuration to add git add command after generating auto-changelog

--- .release-it.js
+++ .release-it.js
@@ -12,13 +12,19 @@ module.exports = {
         commit: true,
         tag: true,
         push: true,
-        commitMessage: "chore(release): ${version}",
+        releaseName: ":rocket: Release ${version}",
+        commitMessage: "chore(release): :rocket: ${version}",
         commitArgs: ["-S"],
         requireCleanWorkingDir: true,
         tagName: "v/${version}",
         tagAnnotation : "Release ${version}",
         tagArgs: ["-s"],
-        changelog: "pnpx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/cookpete/auto-changelog/refs/heads/master/templates/conventional.hbs --tag-prefix=v/"
+        changelog: "pnpx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/cookpete/auto-changelog/refs/heads/master/templates/conventional.hbs --tag-prefix=v/",
+        comments: {
+            submit: false,
+            issue: ":rocket: _This issue has been resolved in v${version}. See [${releaseName}](${releaseUrl}) for release notes._",
+            pr: ":rocket: _This pull request is included in v${version}. See [${releaseName}](${releaseUrl}) for release notes._"
+        }
     },
     github: {
         release: true,
