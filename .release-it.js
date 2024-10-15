a17587e4210bbd315eaad7a616f94d0c5849df27
Joshua <schmitt@unately.com>
2024-10-16 00:20:21 +0200

    refactor: update @release-it/conventional-changelog plugin configuration

--- .release-it.js
+++ .release-it.js
@@ -21,7 +21,7 @@ module.exports = {
     plugins: {
         "@release-it/conventional-changelog": {
             preset: {
-                name: "conventionalcommits",
+                name: "angular",
             },
             infile: "CHANGELOG.md"
         },
