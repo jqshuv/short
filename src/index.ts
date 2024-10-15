b16416e5168220fff01eecebca47c4743f9fdc9d
Joshua <schmitt@unately.com>
2024-10-15 13:40:21 +0200

    Update code formatting settings

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,103 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+function hasValidHeader(request: Request, env: Env): boolean {
+	const searchParams = new URL(request.url).searchParams;
+
+	if (request.headers.has('Authorization')) {
+		const authHeader = request.headers.get('Authorization');
+		if (authHeader === env.AUTH_SECRET) {
+			return true;
+		} else {
+			return false;
+		}
+	} else if (searchParams.has('auth')) {
+		const authParam = searchParams.get('auth');
+		if (authParam === env.AUTH_SECRET) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
+
+function authorizeRequest(request: Request, env: Env): boolean {
+	switch (request.method) {
+		case 'GET':
+			return true;
+		case 'POST':
+			return hasValidHeader(request, env);
+		case 'PUT':
+			return hasValidHeader(request, env);
+		case 'DELETE':
+			return hasValidHeader(request, env);
+		default:
+			return false
+	}
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const requestUrl = new URL(request.url);
+		const shortCodeGet = requestUrl.pathname.slice(1);
+		let body: { shortCode?: string, redirect: string } | undefined;
+
+		// check if request has body and await for it to be parsed to variable body
+
+		if (request.method === 'POST' || request.method === 'PUT') {
+			if (!request.headers.has('Content-Type') || request.headers.get('Content-Type') !== 'application/json') {
+				return new Response('Bad Request', { status: 400 });
+			}
+
+			body = await request.json() as { shortCode?: string, redirect: string } || undefined;
+
+			if (!body || !body.redirect) {
+				return new Response('Bad Request', { status: 400 });
+			}
+		}
+
+		if (!authorizeRequest(request, env) && request.method !== 'GET') {
+			return new Response('Unauthorized', { status: 401 });
+		}
+
+		switch (request.method) {
+			case 'POST':
+				if (!body) return new Response('Bad Request', { status: 400 });
+				const postShortCode = body.shortCode || Math.random().toString(36).slice(2, 8);
+				const postRedirectUrl = body.redirect;
+
+				await env.SHORT_URLS.put(postShortCode, postRedirectUrl);
+				// response with shortCode in json
+				return new Response(JSON.stringify({ shortCode: postShortCode }), { status: 200 });
+			case 'GET':
+				if (!shortCodeGet) return new Response('just redirect. - powered by jqshuv x unately.', { status: 200 });
+
+				const data = await env.SHORT_URLS.get(shortCodeGet);
+				if (data) {
+					return Response.redirect(data, 302);
+				} else {
+					return new Response('Not Found', { status: 404 });
+				}
+			case 'PUT':
+				if (!body) return new Response('Bad Request', { status: 400 });
+				const putShortCode = body.shortCode || Math.random().toString(36).slice(2, 8);
+				const putRedirectUrl = body.redirect;
+
+				await env.SHORT_URLS.put(putShortCode, putRedirectUrl);
+				return new Response(JSON.stringify({ shortCode: putShortCode }), { status: 200 });
+			case 'DELETE':
+				if (!body) return new Response('Bad Request', { status: 400 });
+				const deleteShortCode = body.shortCode
+
+				if (!deleteShortCode) return new Response('Bad Request', { status: 400 });
+
+				await env.SHORT_URLS.delete(deleteShortCode);
+				return new Response('Deleted', { status: 200 });
+			default:
+				return new Response('Method Not Allowed', { status: 405 });
+		}
+
 	},
 } satisfies ExportedHandler<Env>;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,6 @@
 // test/index.spec.ts
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
 
@@ -8,18 +9,88 @@ import worker from '../src/index';
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 describe('Hello World worker', () => {
+	var shortCode = '';
+
 	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
+		const request = new IncomingRequest('http://example.com', { method: 'GET' });
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"just redirect. - powered by jqshuv x unately."`);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+	it('post request url', async () => {
+		const request = new IncomingRequest('http://example.com', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': env.AUTH_SECRET,
+			},
+			body: JSON.stringify({ shortCode: 'test', redirect: 'https://example.com' }),
+		});
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+		await waitOnExecutionContext(ctx);
+		expect(response.status).toBe(200);
 	});
+
+
+
+	it('post request url without shortCode', async () => {
+		const request = new IncomingRequest('http://example.com', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': env.AUTH_SECRET,
+			},
+			body: JSON.stringify({ redirect: 'https://example.com' }),
+		});
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+		await waitOnExecutionContext(ctx);
+		const responseBody = await response.json() as { shortCode: string };
+		shortCode = responseBody.shortCode;
+		expect(responseBody).toMatchObject({ shortCode: shortCode });
+	});
+
+
+
+	it('post request url', async () => {
+		const request = new IncomingRequest('http://example.com', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': env.AUTH_SECRET,
+			},
+			body: JSON.stringify({ shortCode: 'test', redirect: 'https://example.com' }),
+		});
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+		await waitOnExecutionContext(ctx);
+		expect(response.status).toBe(200);
+	});
+
+
+
+	it('post request url without shortCode', async () => {
+		const request = new IncomingRequest('http://example.com', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': env.AUTH_SECRET,
+			},
+			body: JSON.stringify({ redirect: 'https://example.com' }),
+		});
+		const ctx = createExecutionContext();
+		const response = await worker.fetch(request, env, ctx);
+		await waitOnExecutionContext(ctx);
+		const responseBody = await response.json() as { shortCode: string };
+		shortCode = responseBody.shortCode;
+		expect(responseBody).toMatchObject({ shortCode: shortCode });
+	});
+
+
+	console.log(shortCode);
 });
