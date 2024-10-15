b653ef2e565618414ceb014b96da9d082b8a1f97
Joshua <schmitt@unately.com>
2024-10-16 00:17:16 +0200

    refactor: add @release-it/conventional-changelog plugin to package.json

---
+++ .release-it.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2024 Joshua Schmitt
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+module.exports = {
+    git: {
+        commit: true,
+        tag: true,
+        push: true,
+        commitMessage: "chore(release): ${version}",
+        requireCleanWorkingDir: true,
+    },
+    github: {
+        release: true,
+        releaseName: "${version}"
+    },
+    npm: {
+        publish: false,
+    },
+    plugins: {
+        "@release-it/conventional-changelog": {
+            preset: "angular",
+            infile: "CHANGELOG.md"
+        },
+    },
+};
