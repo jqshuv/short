949fd1ff545ec446f02422d000cfde83f1a40dd4
Joshua <schmitt@unately.com>
2024-10-16 02:34:39 +0200

    refactor: release.config.mjs to use 'main' branch instead of 'master'

--- .release-it.js
+++ .release-it.js
@@ -18,7 +18,7 @@ module.exports = {
         tagName: "v/${version}",
         tagAnnotation : "Release ${version}",
         tagArgs: ["-s"],
-        changelog: "pnpx auto-changelog --stdout --commit-limit false -u --template https://raw.githubusercontent.com/cookpete/auto-changelog/refs/heads/master/templates/conventional.hbs --tag-prefix=v/",
+        changelog: "pnpx conventional-changelog-cli --from=${latestTag} --to=${tagName}",
     },
     github: {
         release: true,
