6b4293cbc81f64b116f3390de59fbcd57a517659
Joshua <schmitt@unately.com>
2024-10-20 15:06:15 +0200

    feat: add after:release script to merge main branch into production

--- .release-it.js
+++ .release-it.js
@@ -8,6 +8,7 @@ module.exports = {
     "before:bump": "node scripts/change-api-version.js ${version}",
     // edit the CHANGELOG.md file before the release with sed 's/...main/...v\\/${version}/g'
     "before:release": "pnpx changelogen@latest --from=${latestTag} --output=CHANGELOG.md && sed -i '' 's/...main/...v\\/${version}/g' CHANGELOG.md && git add CHANGELOG.md",
+    "after:release": "git pull --tags && git checkout production && git merge main && git push && git checkout main",
   },
   git: {
     commit: true,
