b663122f5ac151477b4e688426476082d66e53a5
Joshua <schmitt@unately.com>
2024-12-25 23:29:59 +0100

    refactor: remove unnecessary semicolons in change-api-version.js for consistency

--- scripts/change-api-version.js
+++ scripts/change-api-version.js
@@ -3,15 +3,15 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const apiVersion = process.argv[2];
-const apiDocsPath = path.join(__dirname, '..', 'docs', 'api-docs.yaml');
-const apiDocs = fs.readFileSync(apiDocsPath, 'utf8');
+const apiVersion = process.argv[2]
+const apiDocsPath = path.join(__dirname, '..', 'docs', 'api-docs.yaml')
+const apiDocs = fs.readFileSync(apiDocsPath, 'utf8')
 
-const updatedApiDocs = apiDocs.replace(/version: .*/, `version: ${apiVersion}`);
+const updatedApiDocs = apiDocs.replace(/version: .*/, `version: ${apiVersion}`)
 
-fs.writeFileSync(apiDocsPath, updatedApiDocs);
+fs.writeFileSync(apiDocsPath, updatedApiDocs)
 
-console.log(`API version updated to ${apiVersion}`);
+console.log(`API version updated to ${apiVersion}`)
